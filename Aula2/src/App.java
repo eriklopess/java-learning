2342344ae464a7d3a082cc89b8569c233eac9c9c
Erik Lopes <erik.lopesdev@outlook.com>
2021-12-02 19:51:56 -0300

    Operadores Matematicos

---
+++ Aula2/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+        *
+        * + - Soma
+        * - - Subtração
+        * * - Multiplicação
+        * / - Divisão
+        * Uso dos () aumenta a prioridade da expressão
+        *
+        */
+        System.out.println("Hello, World!");
+    }
+}
