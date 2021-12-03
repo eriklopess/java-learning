6bf0fa8249dd1b72f8d52ef39eea82b9df265eeb
Erik Lopes <erik.lopesdev@outlook.com>
2021-12-03 02:24:28 -0300

    Aula 04 Saida de dados em Java

---
+++ Aula-04-saida-de-dados-em-java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!"); // Quebra ao final da linha
+        System.out.print("Good Day"); // Não quenra ao final da linha
+    }
+}
