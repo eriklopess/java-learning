5d3b25129be708c5f2b018172ec9df712ef984cf
Erik Lopes <erik.lopesdev@outlook.com>
2021-12-03 03:24:42 -0300

    Aula-05 Finalizada

---
+++ aula-05-processamento-de-dados-em-java-casting/src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int x, y, z;
+
+        x = 5;
+        y = x * 2;
+        z = y * 2;
+
+        System.out.println(z);
+
+        double b = 6.0, h = 5.0, B = 8.0, area;
+        area = (b + B) / 2 * h;
+
+        System.out.println(area);
+
+        int m, n;
+        double result;
+        m = 5;
+        n = 10;
+
+        result = (double) m * n;
+
+        System.out.println(result);
+
+        /*
+        *
+        * Casting
+        * Casting é a conversão de tipos primitivos.
+        */
+    }
+}
